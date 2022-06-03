332d1e1f4aeb5a7cf6233b4a4d073d4bc70be636
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-03 07:24:30 +0530

    [Nantha] - Multiple Values Added

--- BST/BST/Program.cs
+++ BST/BST/Program.cs
@@ -2,4 +2,16 @@
 BST.BSTFunctions<int> function = new BST.BSTFunctions<int>(56);
 function.Insert(30);
 function.Insert(70);
+function.Insert(3);
+function.Insert(11);
+function.Insert(16);
+function.Insert(67);
+function.Insert(63);
+function.Insert(65);
+function.Insert(60);
+function.Insert(95);
+function.Insert(40);
+function.Insert(22);
 function.Display();
+function.GetSize();
+
